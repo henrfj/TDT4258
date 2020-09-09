d26844120b0f6c847e517e2c63575f6d05ac18eb
henrfj <h1rik.f@gmail.com>
2020-09-09 14:18:18 +0200

    working ex1 part 1: polling

--- exercise1/ex1.s
+++ exercise1/ex1.s
@@ -82,7 +82,54 @@
 	      .type   _reset, %function
         .thumb_func
 _reset: 
-	      b .  // do nothing
+
+		//Activate clk on GPIO_CONTROLLER
+	    ldr r1, =CMU_BASE
+		ldr r2, [r1, #CMU_HFPERCLKEN0]
+
+		mov r3, #1
+		lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
+		orr r2, r2, r3
+
+		str r2, [r1, #CMU_HFPERCLKEN0]
+
+		//Set high drive strength (A)
+		ldr r1, =GPIO_PA_BASE
+		mov r2, #0x2
+		str r2, [r1, #GPIO_CTRL]
+
+		//Setting pins as output (A)
+		mov r2, #0x55555555
+		str r2, [r1, #GPIO_MODEH]
+		
+		//Turn all on (pin 8-15)
+		//now we overwrite pin 0-7 with 00, might want to change that later.
+		mov r2, #0xFF00
+		str r2, [r1, #GPIO_DOUT]
+
+		// Setting pins to low, test
+		mov r2, #0x8C00
+		str r2, [r1, #GPIO_DOUT]
+
+
+		// BUTTONS pins set as input
+		ldr r3, =GPIO_PC_BASE
+		mov r4, #0x33333333
+		str r4, [r3, #GPIO_MODEL]
+
+		//Setting internal pull-ups
+		mov r4, #0xFF
+		str r4, [r3, #GPIO_DOUT]
+
+		loop:
+
+			ldr r4, [r3, #GPIO_DIN]
+			lsl r4, r4, #8
+			str r4, [r1, #GPIO_DOUT]
+
+			b loop
+
+
 	
 	/////////////////////////////////////////////////////////////////////////////
 	//
