09f99ee3915748b3d87f9ecd6e7631982b2d7fac
glemco <glemco@aol.com>
2020-10-02 19:12:55 +0200

    Added python midi parser and updated songs header

---
+++ exercise2/midi/midi_parser.py
@@ -0,0 +1,74 @@
+import sys
+from os import path
+import mido
+
+A = 1.059463094359
+F_0 = 440
+NOTE_0 = 69 # A4 where C4 is 60
+NOTE_NAMES = ("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+
+CPU_FREQ = 14000000
+
+get_freq = lambda note: round(F_0 * pow(A, note - NOTE_0), 3)
+
+if len(sys.argv) < 2:
+    print("Usage {} filename.mid".format(sys.argv[0]))
+    sys.exit(1)
+
+with open('songs.h', 'w') as ofile:
+    ofile.write('#ifndef _SONGS_H\n#define _SONGS_H\n\n')
+    ofile.write('#define SONG(id) (\\\n')
+    for ind, file_name in enumerate(sys.argv[1:]):
+        ofile.write('\t\tid=={} ? song{} : \\\n'.format(str(ind), str(ind)))
+    ofile.write('\t\tsong0) //default\n')
+    ofile.write('#define SONG_AMPL(id) (\\\n')
+    for ind, file_name in enumerate(sys.argv[1:]):
+        ofile.write('\t\tid=={} ? song{}_ampl : \\\n'.format(str(ind), str(ind)))
+    ofile.write('\t\tsong0_ampl) //default\n')
+    for ind, file_name in enumerate(sys.argv[1:]):
+        if not file_name.endswith('.mid'):
+            print("Mid file not provided, please pass a file with extension `mid`")
+            sys.exit(2)
+        mid = mido.MidiFile(file_name)
+        notes = []
+        velocities = []
+        for track in mid.tracks:
+            #print('Track {}: {}'.format(i, track.name))
+            for msg in track:
+                if msg.type == 'note_on':
+                    note = msg.note
+                    freq = get_freq(note)
+                    octave = (note//12) - 1
+                    name = NOTE_NAMES[note % 12]
+                    divider = int(CPU_FREQ // (2*freq)) #double frequency to get both edges
+                    print("{} ({} {}) at {} Hz ({})".format(note, name, octave, freq, divider))
+                    #convert later
+                    notes.append(freq)
+                    velocities.append(msg.velocity)
+                #if msg.type == 'note_off':
+                #    print(msg)
+
+        max_v = max(velocities)
+        min_v = min(velocities)
+        for i, v in enumerate(velocities):
+            velocities[i] = 14 * (v-min_v) // (max_v - min_v) + 1
+
+        def write_array(ofile, data_type, arr_name, arr, cols):
+            ofile.write('const {} {}[{}] = {{\n\t'
+                        .format(data_type, arr_name, len(arr)+1))
+            for i, n in enumerate(arr):
+                if i % cols == cols-1:
+                    ofile.write('{},\n\t'.format(n))
+                elif i == len(arr)-1:
+                    ofile.write('{}'.format(n))
+                else:
+                    ofile.write('{}, '.format(n))
+            ofile.write('\n};\n')
+
+        name = 'song' + str(ind)
+        ofile.write('\n//{}\n'.format(path.splitext(path.basename(file_name))[0]))
+        notes.insert(0, len(notes))
+        write_array(ofile, 'float', name, notes, 7)
+        velocities.insert(0, 0) #length not used here (and may not fit), same as notes
+        write_array(ofile, 'uint8_t', name+'_ampl', velocities, 15)
+    ofile.write('\n#endif\n')
