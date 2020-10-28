02f07f3c9bb50bd5d53eb99599447b4ab28ab55e
Gabriele Monaco <glemco@aol.com>
2020-10-28 15:42:38 +0100

    Copied driver skeleton from sound driver to gamepad

--- exercise3/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ exercise3/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -8,7 +8,7 @@
 #include "efm32gg.h"
 
 /*
- * template_init - function to insert this module into kernel space
+ * gamepad_init - function to insert this module into kernel space
  *
  * This is the first of two exported functions to handle inserting this
  * code into a running kernel
@@ -16,7 +16,21 @@
  * Returns 0 if successfull, otherwise -1
  */
 
-static int __init template_init(void)
+/* Global attributes */
+struct class *gamepad_cls;
+dev_t devno;
+struct cdev gamepad_cdev;
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.read = gamepad_read,
+	.write = gamepad_write,
+	.open = gamepad_open,
+	.release = gamepad_release
+};
+char buttons_value; //FIXME put that somewhere else
+
+static int __init gamepad_init(void)
 {
 	printk("Hello World, here is your module speaking\n");
 	//Enable GPIO clock
@@ -37,23 +51,87 @@ static int __init template_init(void)
 	//*GPIO_EXTIRISE = 0xff;	//enable generation on rise
 	*GPIO_EXTIFALL = 0xff;	//enable generation on fall
 
+	//character device allocations
+	err	= alloc_chrdev_region(&devno, 0, DEVCNT, DEVNAME);
+	if(err<0) {
+		printk(KERN_WARNING"Error while allocating the device (%d)\n", err);
+		return err;
+	}
+	cdev_init(&dev.gamepad_cdev, &fops);
+	dev.gamepad_cdev.owner = THIS_MODULE;
+	dev.gamepad_cdev.ops = &fops;
+	err = cdev_add(&dev.gamepad_cdev, devno, 1);
+	if(err<0) {
+		printk(KERN_WARNING"Error while adding the char device %s (%d)\n", DEVNAME, err);
+	}
+
+	gamepad_cls = class_create(THIS_MODULE, DEVNAME);
+	if(!gamepad_cls) {
+		printk(KERN_WARNING"Error while creating the device class for %s\n", DEVNAME);
+		return -1;
+	}
+	gamepad_cls->devnode = gamepad_devnode;
+
+	if(!device_create(gamepad_cls, NULL, devno, NULL, DEVNAME)) {
+		printk(KERN_WARNING"Error while creating the device for %s\n", DEVNAME);
+		return -1;
+	}
+
 	return 0;
 }
 
+static int gamepad_open(struct inode *inode, struct file *filp) {
+	printk(KERN_INFO"Device file has been opened\n");
+	return 0;
+}
+
+static int gamepad_release(struct inode *inode, struct file *filp) {
+	printk(KERN_INFO"Device file has been closed\n");
+	return 0;
+}
+
+static ssize_t gamepad_read(struct file *filp, char __user *buff, size_t count, loff_t *offp) {
+	buttons_value = 0xff; //TODO implement the interrupt to populate this (and remove this line)
+	printk(KERN_INFO"Buttons value is %x\n", buttons_value);
+	if(*offp >= 1) return 0; //EOF
+	if(copy_to_user(buff, &buttons_value, 1)) {
+		printk(KERN_WARNING"Missing some bytes, failing\n");
+		return -EFAULT;
+	}
+	*offp++; //1 byte read
+	return READ_SIZE;
+}
+
+static ssize_t gamepad_write(struct file *filp, const char __user *buff, size_t count, loff_t *offp) {
+	char val;
+	if(*offp >= 1) return 0; //EOF
+	if(copy_from_user(&val, buff, 1)) {
+		printk(KERN_WARNING"Missing some bytes, failing\n");
+		return -EFAULT;
+	}
+	printk(KERN_INFO"User value is %x\n", val); //dummy thing
+	offp++;
+	return count; //fake to read it all
+}
+
 /*
- * template_cleanup - function to cleanup this module from kernel space
+ * gamepad_cleanup - function to cleanup this module from kernel space
  *
  * This is the second of two exported functions to handle cleanup this
  * code from a running kernel
  */
 
-static void __exit template_cleanup(void)
+static void __exit gamepad_cleanup(void)
 {
-	 printk("Short life for a small module...\n");
+	 printk(KERN_INFO"Cleaning up the devices\n");
+	 device_destroy(sound_cls, devno);
+	 class_destroy(sound_cls);
+	 cdev_del(&dev.sound_cdev);
+	 unregister_chrdev_region(devno, DEVCNT);
 }
 
-module_init(template_init);
-module_exit(template_cleanup);
+module_init(gamepad_init);
+module_exit(gamepad_cleanup);
 
 MODULE_DESCRIPTION("Small module, demo only, not very useful.");
 MODULE_LICENSE("GPL");
