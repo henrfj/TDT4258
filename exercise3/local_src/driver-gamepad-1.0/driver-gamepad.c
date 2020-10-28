bd16c02553ef242188c4abf91379aee033c06231
henrfj <h1rik.f@gmail.com>
2020-10-28 13:54:07 +0100

    started work on gamepad driver

--- exercise3/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ exercise3/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -18,6 +18,24 @@
 static int __init template_init(void)
 {
 	printk("Hello World, here is your module speaking\n");
+	//Enable GPIO clock
+	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;
+
+	//BUTTONS
+	*GPIO_PC_MODEL = 0x33333333;	/*Setting pins as input pins */
+	*GPIO_PC_DOUT = 0xff;	/*Internal pull-ip resistors for the buttons */
+
+	//Interrupt handling GPIO
+
+	//Setting BUTTON port to allow interrupts (Port C)
+	*GPIO_EXTIPSELL = 0x22222222;
+
+	//Enables interrupts on rise and fall values, and interrupt generation
+	// from the GPIO
+	*GPIO_IEN = 0xff;	//enable generation
+	//*GPIO_EXTIRISE = 0xff;	//enable generation on rise
+	*GPIO_EXTIFALL = 0xff;	//enable generation on fall
+
 	return 0;
 }
 
