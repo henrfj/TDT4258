4a5ec21411ca5efc69c48947522d6f7771d4b9db
Nicolai <dalaaker.nicolai@gmail.com>
2020-11-08 20:11:17 +0100

    added scaffolding for possible snake tail solution

--- exercise3/local_src/game-1.0/game.c
+++ exercise3/local_src/game-1.0/game.c
@@ -15,6 +15,7 @@ void game_loop(){
 	int fruitPos[2];
 	int direction;
 	int newDirection;
+	int snakeBody[2][0] //keeps track of the snake - might be switched with a queue if there exists one
 
 	while(1){
 		//clear board, set alive
@@ -23,6 +24,7 @@ void game_loop(){
 		int alive = 1;
 		headPos = [15, 15]
 		direction = RIGHT;
+		int keepTail; //for reorder_snake() solution, should be removed if not used
 		// fruitPos == generateFruit()
 		while(alive){
 			/* Checks if direction has changed, and if so, changes direction*/
@@ -47,17 +49,26 @@ void game_loop(){
 			/*checks the new snake heads position */
 			if(headPos[0] == fruitPos[0] && headPos[1] == fruitPos[1] ){
 				board[headPos[0]][headPos[1]] == 1; // adds the new snake head to the board(hence increasing the snake)
+				keepTail=true;
 				//TODO: play eating sound here
 				//TODO: fruitPos == generateFruit()
 			}else if (board[headPos[0]][headPos[1]] == 1){
 				alive = 0;
+				keepTail=false;
 				//TODO: play dead sound here
 			}else if ([headPos[0] > BOARD_SIZE || headPos[0] < 0 || headPos[0] > BOARD_SIZE || headPos[0] < 0)
-				//TODO: snake dies? add same logic as above if that is the case
+				alive=0;
+				keepTail=false
+				
+				//snake dies?
+				//TODO: play dead sound here
 			}else
 				//TODO: remove end of snake! Ups, we need to know the position,and be able to update it!
 				//Do we need a new datastructure? How about a linked list, going from snake end to head?
+				//posible solution 
 			}
+			//reorder_snake(&head, &snakeBody, &gameBoard, keepTail)
+			
 			
 
 			//run_gamepad_engine();
@@ -73,6 +84,14 @@ void game_loop(){
 		}
 	}
 
+}
+
+//hacky solution, requires o(n) iterations where n is snake length for each game tick.
+void reorder_snake(int[] *head, int[][] *snakeBody, int [][] *gameBoard, int keepTail){
+	/*shift all elements index by one,
+	remove the last element if keepTail is true
+	set index 0 to head.
+	set 0 at gameboard position of the tail.*/
 }
 
 void get_input(){
