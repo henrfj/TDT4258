3ed69824fe9fd1f5719af9467038f75600fd7e31
Gabriele Monaco <gabriemo@stud.ntnu.no>
2020-11-09 12:24:53 +0100

    Starting to work on the screen_engine, still not working

--- exercise3/local_src/game-1.0/screen_engine.c
+++ exercise3/local_src/game-1.0/screen_engine.c
@@ -1,1 +1,37 @@
 #include "screen_engine.h"
+#include "foo.h"
+
+void print_image() {
+	uint16_t i, j;
+	uint16_t *screen;
+	int f_screen = open("/dev/fb0", O_RDWR);
+	if(f_screen < 0) {
+		fprintf(stderr, "Cannot open screen\n");
+		exit(-1);
+	}
+	screen = mmap(NULL, SCREEN_SIZE*sizeof(uint16_t),
+			PROT_READ | PROT_WRITE,
+			MAP_PRIVATE | MAP_ANONYMOUS, f_screen, 0);
+	if(screen == MAP_FAILED) {
+		fprintf(stderr, "Cannot map screen error %s\n", strerror(errno));
+		return;
+	}
+	printf("Printing an image to the screen\n");
+	for(i=0; i<IMG_X; i++) {
+		for(j=0; j<IMG_X; j++) {
+			screen[i + j*SCREEN_Y] = peach[i][j];
+			//if(write(f_screen, peach[i], IMG_X*sizeof(uint16_t)) <
+			//		IMG_X * sizeof(uint16_t))
+			//	printf("Wrong write..\n");
+		}
+	}
+	struct fb_copyarea rect;
+	rect.dx = 0;
+	rect.dy = 0;
+	rect.width = SCREEN_X;
+	rect.height = SCREEN_Y;
+	ioctl(f_screen, 0x4680, &rect);
+
+	munmap(screen, SCREEN_SIZE*sizeof(uint16_t));
+	close(f_screen);
+}
--- exercise3/local_src/game-1.0/screen_engine.h
+++ exercise3/local_src/game-1.0/screen_engine.h
@@ -1,5 +1,22 @@
 #ifndef SCREEN_ENGINE_H
 #define SCREEN_ENGINE_H
 
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <inttypes.h>
+#include <errno.h>
+#include <linux/fb.h>
+
+#define SCREEN_X 320
+#define SCREEN_Y 240
+#define SCREEN_SIZE (SCREEN_X*SCREEN_Y)
+
+#define IMG_X 20
+#define IMG_Y IMG_X
+
+void print_image();
 
 #endif
